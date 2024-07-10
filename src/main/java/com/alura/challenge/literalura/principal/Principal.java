44e7ea1d385eb494bfc10982a898a8695b505bd6
zotel1 <zotelsigel@gmail.com>
2024-07-10 20:48:08 -0300

    update: trabajo terminado :)

--- src/main/java/com/alura/challenge/literalura/principal/Principal.java
+++ src/main/java/com/alura/challenge/literalura/principal/Principal.java
@@ -9,7 +9,9 @@ import com.alura.challenge.literalura.repository.AutorRepository;
 import com.alura.challenge.literalura.repository.LibrosRepository;
 import com.alura.challenge.literalura.service.ConsumoAPI;
 import com.alura.challenge.literalura.service.ConvierteDatos;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.*;
@@ -55,17 +57,17 @@ public class Principal {
                 case 1:
                     buscarLibroPorTitulo();
                     break;
-              //  case 2:
-//                    listarLibrosRegistrados();
-  //                  break;
-    //            case 3:
-      //              listarAutoresRegistrados();
-        //        case 4:
-          //          ListarAutoresVivos();
-            //        break;
-              //  case 5:
-                //    listarLibrosPorIdioma();
-                  //  break;
+                case 2:
+                    listarLibrosRegistrados();
+                    break;
+                case 3:
+                    listarAutoresRegistrados();
+                case 4:
+                    listarAutoresVivos();
+                    break;
+                case 5:
+                    listarLibrosPorIdioma();
+                    break;
                 case 0:
                     System.out.println("Cerrando la aplicación...");
                     break;
@@ -73,7 +75,7 @@ public class Principal {
                     System.out.println("Opcion inválida");
             }
         }
-        }
+    }
 
         private DatosLibros getDatosLibros() {
             System.out.println("Ingrese el nombre del libro que quiere buscar: ");
@@ -106,6 +108,92 @@ public class Principal {
         System.out.println(libros.toString());
     }
 
+    @Transactional(readOnly = true) // Esta anotación nos es útil cuando queremos asegurarnos de que un método no realice modificaciones en la base de datos y solo realice consultas
+    private void listarLibrosRegistrados() {
+        libros = new HashSet<>(librosRepository.findAll());
+
+        libros.forEach(libro -> {
+            librosRepository.findByIdWithAutoresAndIdiomas(libro.getId()).ifPresent(librosConAutores -> {
+                libro.setAutores(librosConAutores.getAutores());
+                libro.setIdiomas(librosConAutores.getIdiomas());
+            });
+        });
+
+        // Comparamos todo para ordenar los libros por el nombre del primer autor
+        Comparator<Libros> comparador = Comparator.comparing(libro ->
+                libro.getAutores().isEmpty() ? "" : libro.getAutores().iterator().next().getNombre());
+        libros.stream()
+                .sorted(comparador)
+                .forEach(System.out::println);
     }
 
+    @Transactional(readOnly = true)
+    private void listarAutoresRegistrados() {
+        List<Autor> autores = autorRepository.findAllWithLibros();
+        if (autores.isEmpty()) {
+            System.out.println("No hay autores registrados.");
+        } else {
+            autores.forEach(autor -> {
+                System.out.println("Autor: " + autor.getNombre());
+                System.out.println("Fecha de Nacimiento: " + autor.getVive());
+                System.out.println("Fecha de Fallecimiento: " + (autor.getNoVive() != null ? autor.getNoVive() : "N/A"));
+                if (autor.getLibros() != null) {
+                    System.out.println("Libro: " + autor.getLibros().getTitulo());
+                } else {
+                    System.out.println("Libro: N/A");
+                }
+                System.out.println();
+            });
+        }
+    }
 
+    @Transactional(readOnly = true)
+    private void listarAutoresVivos() {
+        System.out.println("Por favor escribe el año que desees buscar: ");
+        var anio = teclado.nextInt();
+        teclado.nextLine();
+        List<Autor> autores = autorRepository.findByviveLessThanEqualAndnoViveGreaterThanEqual(anio);
+        if (autores.isEmpty()) {
+            System.out.println("No se pudo encontrar a un autor que haya vivido en el año: " + anio);
+        } else {
+            autores.forEach(autor -> {
+                System.out.println("Nombre: " + autor.getNombre());
+                System.out.println("Fecha de Nacimiento: " + autor.getVive());
+                System.out.println("Fecha de Fallecimiento: " + (autor.getNoVive() != null ? autor.getNoVive() : "N/A"));
+                System.out.println("libro: " + (autor.getLibros() != null ? autor.getLibros().getTitulo() : "N/A"));
+            });
+        }
+    }
+
+    @Transactional(readOnly = true)
+    private void listarLibrosPorIdioma() {
+        System.out.println("Por favor ingrese el idioma en el cual desea buscar los libros (es, en fr, pt, etc.)");
+        var lenguajeIngresado = teclado.nextLine().toLowerCase();
+
+        try {
+            Lenguajes lenguajes = Lenguajes.fromString(lenguajeIngresado);
+            List<Libros> libros = librosRepository.findByIdiomas(lenguajes);
+
+            if (libros.isEmpty()) {
+                System.out.println("No se encontraron libros en el idioma: " + lenguajeIngresado );
+            } else {
+                libros.forEach(libro -> {
+                    Hibernate.initialize(libro.getAutores());
+                    Hibernate.initialize(libro.getIdiomas());
+
+                    System.out.println(
+                            "########### Libro ##########" +
+                                    "\nTitulo: " + libro.getTitulo() +
+                                    "\nAutores: " + libro.getAutores().stream().map(Autor::getNombre).collect(Collectors.joining(", ")) +
+                                    "\nIdiomas: " + libro.getIdiomas().stream().map(Lenguajes::getDescripcion).collect(Collectors.joining(", ")) +
+                                    "\nNúmero de descargas: " + libro.getCantidadDescargas() +
+                                    "\n#################### "
+                    );
+                });
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Idioma no reconocido: " + lenguajeIngresado);
+        }
+    }
+
+}
